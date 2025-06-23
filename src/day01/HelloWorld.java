41103904b64a6f18512e5062c15a4d48e770b5c4
asli40 <170577615+asli40@users.noreply.github.com>
2025-06-23 11:29:06 -0700

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+        
+        
+        
+        
+        
     }
 }
